07203ecc189455b9fbe5ad76759415161abe64fc
kenji-tojo <kenjitojo@icloud.com>
2022-05-30 20:22:33 +0900

    refactor

--- report1/main.cpp
+++ report1/main.cpp
@@ -5,124 +5,91 @@
 
 #include "Eigen/Dense"
 
-inline double eval_f(
-        const Eigen::MatrixXd &A,
-        const Eigen::VectorXd &b,
-        const double lambda,
-        const Eigen::VectorXd &w)
-{
-    return (b - A * w).squaredNorm() + lambda * w.squaredNorm();
-}
-
-void steepest_descent(
-        const Eigen::MatrixXd &A,
-        const Eigen::VectorXd &b,
-        const double lambda,
-        const int niter,
-        std::ofstream &ofs)
-{
-    const auto n = A.cols();
-
-    auto C = 2.0 * (A.transpose() * A + lambda * Eigen::MatrixXd::Identity(n,n));
-    auto d = 2.0 * A.transpose() * b;
-
-    auto alpha = 1.0 / C.operatorNorm();
-    std::cout << "using constant alpha" << std::endl;
-
-    Eigen::VectorXd w_star = C.colPivHouseholderQr().solve(d);
-    const auto f_star = eval_f(A, b, lambda, w_star);
-    ofs << "niter: "  << niter << ",";
-    ofs << "lambda: " << lambda << ",";
-    ofs << "f_star: " << f_star << std::endl;
-    Eigen::VectorXd w(n); w.setZero();
-    for (int iter=0;iter<niter;iter++) {
-        auto grad = C * w - d;
-        w = w - alpha * grad;
-        auto f = eval_f(A, b, lambda, w);
-        ofs << f << "," << std::abs(f_star - f) << std::endl;
+class SteepestDescent {
+public:
+    SteepestDescent(Eigen::MatrixXd A, Eigen::VectorXd b)
+            : m_A(std::move(A))
+            , m_b(std::move(b))
+    {
+        init();
     }
-}
 
-void steepest_descent_armijo(
-        const Eigen::MatrixXd &A,
-        const Eigen::VectorXd &b,
-        const double lambda,
-        const int niter,
-        const double xi,
-        const double tau,
-        const int nbacktrack,
-        std::ofstream &ofs)
-{
-    const auto n = A.cols();
+    [[nodiscard]]
+    inline double eval(const Eigen::VectorXd &w) const {
+        return (m_b - m_A * w).squaredNorm() + m_lambda * w.squaredNorm();
+    }
 
-    auto C = 2.0 * (A.transpose() * A + lambda * Eigen::MatrixXd::Identity(n,n));
-    auto d = 2.0 * A.transpose() * b;
+    inline void init() {
+        m_C = 2.0 * (m_A.transpose() * m_A + m_lambda * Eigen::MatrixXd::Identity(m_A.cols(),m_A.cols()));
+        m_d = 2.0 * m_A.transpose() * m_b;
+        m_inv_L = 1.0 / m_C.operatorNorm();
+        m_w_star = m_C.colPivHouseholderQr().solve(m_d);
+        m_f_star = eval(m_w_star);
+    }
 
-    std::cout << "using Armijo criteria" << std::endl;
+    inline void set_lambda(double lambda) {
+        m_lambda = lambda < 0 ? 0 : lambda;
+        init();
+    }
 
-    Eigen::VectorXd w_star = C.colPivHouseholderQr().solve(d);
-    const auto f_star = eval_f(A, b, lambda, w_star);
-    ofs << "niter: "  << niter << ",";
-    ofs << "lambda: " << lambda << ",";
-    ofs << "f_star: " << f_star << std::endl;
-    Eigen::VectorXd w(n); w.setZero();
-    Eigen::VectorXd w_tmp = w;
-    for (int iter=0;iter<niter;iter++) {
-        auto grad = C * w - d;
-        Eigen::VectorXd dir = -grad;
-        dir.normalize();
-        auto gdd = grad.dot(dir);
-        auto alpha = 1.0;
-        auto f = eval_f(A, b, lambda, w);
+    [[nodiscard]]
+    double lambda() const { return m_lambda; }
+
+    void step(Eigen::VectorXd &w) {
+        auto grad = m_C * w - m_d;
+        w = w - m_inv_L * grad;
+    }
+
+    struct ArmijoParams {
+        double alpha_zero = 1;
+        double tau = 0.5;
+        double xi = 1e-3;
+        int iter_max = 50;
+    };
+
+    void step_Armijo(Eigen::VectorXd &w, const ArmijoParams &prm)
+    {
+        auto grad = m_C * w - m_d;
+        auto grad2 = grad.squaredNorm();
+        auto f = eval(w);
         auto f_tmp = f;
-        for (int l=0;l<nbacktrack;l++) {
-            w_tmp = w + alpha * dir;
-            f_tmp = eval_f(A, b, lambda, w_tmp);
-            if (f_tmp <= f + xi * alpha * gdd) {
+        auto w_tmp = w;
+        auto alpha = prm.alpha_zero;
+        for (int l=0;l<prm.iter_max;l++) {
+            w_tmp = w - alpha * grad;
+            f_tmp = eval(w_tmp);
+            if (f_tmp <= f - prm.xi * alpha * grad2) {
                 break;
             }
-            alpha *= tau;
+            alpha *= prm.tau;
         }
         w = w_tmp;
-        f = f_tmp;
-        ofs << f << "," << std::abs(f_star - f) << std::endl;
     }
-}
 
-void steepest_descent_nesterov(
-        const Eigen::MatrixXd &A,
-        const Eigen::VectorXd &b,
-        const double lambda,
-        const int niter,
-        std::ofstream &ofs)
-{
-    const auto n = A.cols();
-
-    auto C = 2.0 * (A.transpose() * A + lambda * Eigen::MatrixXd::Identity(n,n));
-    auto d = 2.0 * A.transpose() * b;
-
-    auto alpha = 1.0 / C.operatorNorm();
-    std::cout << "using Nesterov acceleration" << std::endl;
-
-    Eigen::VectorXd w_star = C.colPivHouseholderQr().solve(d);
-    const auto f_star = eval_f(A, b, lambda, w_star);
-    ofs << "niter: "  << niter << ",";
-    ofs << "lambda: " << lambda << ",";
-    ofs << "f_star: " << f_star << std::endl;
-    Eigen::VectorXd w(n); w.setZero();
-    Eigen::VectorXd w_prev = w;
-    Eigen::VectorXd y = w;
-    for (int iter=0;iter<niter;iter++) {
-        auto grad = C * y - d;
-        auto k = iter + 1;
+    void step_Nesterov(Eigen::VectorXd &w, Eigen::VectorXd &y, const int k) {
+        auto grad = m_C * y - m_d;
         auto beta = (double)k / (double)(k+3);
-        w_prev = w;
-        w = y - alpha * grad;
+        auto w_prev = w;
+        w = y - m_inv_L * grad;
         y = w + beta * (w - w_prev);
-        auto f = eval_f(A, b, lambda, w);
-        ofs << f << "," << std::abs(f_star - f) << std::endl;
     }
-}
+
+    [[nodiscard]]
+    inline double f_star() const { return m_f_star; }
+
+protected:
+    Eigen::MatrixXd m_A;
+    Eigen::VectorXd m_b;
+    double m_lambda = 0;
+    double m_inv_L = 1;
+
+    Eigen::MatrixXd m_C;
+    Eigen::VectorXd m_d;
+
+    Eigen::VectorXd m_w_star;
+    double m_f_star = 0;
+
+};
 
 int main() {
 
@@ -143,22 +110,61 @@ int main() {
     }
     const auto lambdas = std::vector<double>{ 0, 1, 10.0 };
 
+    SteepestDescent optim(A, b);
+    Eigen::VectorXd w(n), y(n);
+
     std::ofstream ofs;
     ofs.open("data/q1.txt", std::ios::out);
     for (const auto lambda : lambdas) {
-        steepest_descent(A, b, lambda, niter, ofs);
+        optim.set_lambda(lambda);
+        ofs << "niter: "  << niter << ",";
+        ofs << "lambda: " << optim.lambda() << ",";
+        ofs << "f_star: " << optim.f_star() << std::endl;
+
+        w.setZero();
+        for (int i=0;i<niter;i++) {
+            optim.step(w);
+            auto f = optim.eval(w);
+            ofs << f << "," << f - optim.f_star() << std::endl;
+        }
     }
     ofs.close();
 
     ofs.open("data/q2.txt", std::ios::out);
-    for (const auto lambda : lambdas) {
-        steepest_descent_armijo(A, b, lambda, niter, 1e-3, 0.5, 50, ofs);
+    {
+        SteepestDescent::ArmijoParams prm;
+        prm.alpha_zero = 1;
+        prm.tau = 0.5;
+        prm.xi = 1e-3;
+        for (const auto lambda : lambdas) {
+            optim.set_lambda(lambda);
+            ofs << "niter: "  << niter << ",";
+            ofs << "lambda: " << optim.lambda() << ",";
+            ofs << "f_star: " << optim.f_star() << std::endl;
+
+            w.setZero();
+            for (int i=0;i<niter;i++) {
+                optim.step_Armijo(w, prm);
+                auto f = optim.eval(w);
+                ofs << f << "," << f - optim.f_star() << std::endl;
+            }
+        }
     }
     ofs.close();
 
     ofs.open("data/q3.txt", std::ios::out);
     for (const auto lambda : lambdas) {
-        steepest_descent_nesterov(A, b, lambda, niter, ofs);
+        optim.set_lambda(lambda);
+        ofs << "niter: "  << niter << ",";
+        ofs << "lambda: " << optim.lambda() << ",";
+        ofs << "f_star: " << optim.f_star() << std::endl;
+
+        w.setZero(); y.setZero();
+        for (int i=0;i<niter;i++) {
+            optim.step_Nesterov(w, y, i+1);
+            auto f = optim.eval(w);
+            ofs << f << "," << f - optim.f_star() << std::endl;
+        }
     }
     ofs.close();
 
